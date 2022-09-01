dbe19e936bbff5e30f641b7a6d9cd876cefdd784
QuentinMerabet <quentin.merabet@yahoo.com>
2022-09-01 05:05:07 -0400

    fix viewer link

--- index.js
+++ index.js
@@ -385,7 +385,7 @@ app.get("/:tokenId", async (req, res) => {
     const metadata = {
       name: `SoulX Passport #${req.params.tokenId}`,
       image: "https://soulx.org/img/cover.gif",
-      animation_url: `https://viewer.soulx.org/?id=${req.params.tokenId}`,
+      animation_url: `https://soulx-animation.vercel.app/?id=${req.params.tokenId}`,
       attributes: [
         { value: passport.name, trait_type: "Username" },
         {
